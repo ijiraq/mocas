bbfd621a80bba4d42258aad8d57086fb31a521de
JJ Kavelaars <jjkavelaars@gmail.com>
2023-10-28 15:52:51 -0700

    Make the live test a little shorter in duration.

--- test/test_core.py
+++ test/test_core.py
@@ -5,13 +5,13 @@ import warnings
 from mocas import core
 from mp_ephem import EphemerisReader
 
+ast_filename = 'data/test.ast'
 
 class Test(TestCase):
     def test_search(self):
         warnings.simplefilter('ignore', category=UserWarning)
-        ast_file = 'data/0123T02.ast'
-        observations = EphemerisReader().read(ast_file)
-        ssois_table = core.search(ast_file, start_date=observations[0].date, end_date=observations[-1].date)
+        observations = EphemerisReader().read(ast_filename)
+        ssois_table = core.search(ast_filename, start_date=observations[0].date, end_date=observations[-1].date)
         for observation in observations:
             frame = observation.comment.frame.split('p')[0]+'p'
             self.assertTrue(numpy.sum(ssois_table['Image'] == frame) > 0)
